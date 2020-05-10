22843baef38afd9d26663160886661d21dd8a9bb
Shahbaz Mogal <mogals@msoe.edu>
2020-05-10 15:46:45 -0500

    Shahnawaz Basic Template: Demo of Connect, Read, Write, Display. Techstack: Firebase + React + Semantic UI React/Bootstrap

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { Header } from "semantic-ui-react";
+import { Input } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 export class App extends Component {
   constructor() {
@@ -11,12 +13,13 @@ export class App extends Component {
     this.state = {
       backlog_items: [],
       taskname: "",
+      backlog_input: "",
     };
+    this.database = firebase.firestore();
   }
 
   getTasks = () => {
-    firebase
-      .firestore()
+    this.database
       .collection("todo-items")
       .get()
       .then((querySnapshot) => {
@@ -25,21 +28,44 @@ export class App extends Component {
             backlog_items: doc.data().backlog_items,
             taskname: doc.data().taskname,
           });
-          console.log(doc.data());
         });
       });
   };
 
-  componentDidMount() {
-    this.getTasks();
-  }
+  addToBacklog = (e) => {
+    console.log("Submitted: " + this.state.backlog_input);
+    this.database
+      .collection("todo-items")
+      .add({ backlog_items: this.state.backlog_input })
+      .then((data) => {
+        console.log("Data: ", data);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
+  updateBacklogInput = (e) => {
+    this.setState({ backlog_input: e.target.value });
+  };
 
   render() {
+    this.getTasks();
     return (
       <div>
-        <Header as="h2">{"Taskname: " + this.state.taskname}</Header>
-
+        <Header as="h2" textAlign="center">
+          <Header.Content> {this.state.taskname}</Header.Content>
+        </Header>
         <Header as="h4">{"Backlog: " + this.state.backlog_items + "\n"}</Header>
+        <Form onSubmit={this.addToBacklog}>
+          <Form.Field>
+            <label>Enter new Backlog Item: </label>
+            <input
+              onChange={this.updateBacklogInput}
+              placeholder="Merge Branches"
+            />
+          </Form.Field>
+        </Form>
       </div>
     );
   }

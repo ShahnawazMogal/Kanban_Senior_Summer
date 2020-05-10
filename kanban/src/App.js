6a855ae7676df7833df5bbb9f7afff2fd0063e84
Shahbaz Mogal <mogals@msoe.edu>
2020-05-10 03:24:51 -0500

    Finished basic demo of Firebase + React

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { Header } from "semantic-ui-react";
 
-export default class App extends Component {
+export class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: [],
-      title: "",
-      description: "",
+      backlog_items: [],
+      taskname: "",
     };
   }
 
   getTasks = () => {
     firebase
       .firestore()
-      .collection("tasks")
+      .collection("todo-items")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
           this.setState({
-            data: [...this.state.data, doc.data()],
+            backlog_items: doc.data().backlog_items,
+            taskname: doc.data().taskname,
           });
+          console.log(doc.data());
         });
       });
   };
@@ -30,12 +34,15 @@ export default class App extends Component {
     this.getTasks();
   }
 
-  render(){
-    return(
-      {getTasks()}
-    )
-  }
+  render() {
+    return (
+      <div>
+        <Header as="h2">{"Taskname: " + this.state.taskname}</Header>
 
+        <Header as="h4">{"Backlog: " + this.state.backlog_items + "\n"}</Header>
+      </div>
+    );
+  }
 }
 
 export default App;
